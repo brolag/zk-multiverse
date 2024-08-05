da7186fed06e66e445ddcd3e7dc103c0c53c45c9
Alfredo Bonilla <info@alfredobonilla.com>
2024-08-05 15:33:02 -0600

    feat: add zk multiverse assets

---
+++ packages/nextjs/components/assets/ZKMultiverseLogo.tsx
@@ -0,0 +1,16 @@
+import { Letter } from "./Letter";
+
+const ZKMultiverseLogo = () => {
+  const title = "ZK MULTIVERSE";
+  return (
+    <div className="flex flex-row space-x-3">
+      {title
+        .split("")
+        .map((letter, index) =>
+          letter === " " ? <div key={index} className="w-20 h-20"></div> : <Letter key={index} letter={letter} />,
+        )}
+    </div>
+  );
+};
+
+export default ZKMultiverseLogo;
