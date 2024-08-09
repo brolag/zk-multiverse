b371369114bd9cf44abb9fa4983234e8ea0fb67f
Alfredo Bonilla <info@alfredobonilla.com>
2024-08-08 18:16:25 -0600

    feat: add styles to the statement block

--- packages/nextjs/app/engineering/circom/[slug]/page.tsx
+++ packages/nextjs/app/engineering/circom/[slug]/page.tsx
@@ -32,17 +32,17 @@ export default async function Page({ params }: PageProps) {
           </h2>
         </div>
 
-        <div className="w-full max-w-[64rem] p-4 bg-gray-800 rounded-lg mb-6 font-play">
+        <div className="w-full max-w-[64rem] p-4 bg-gray-700 rounded-lg mb-6 font-play shadow-lg">
           <div className="mb-4">
             <h3 className="text-white text-xl font-bold">Statements</h3>
-            <ul className="text-white">
+            <ul className="text-gray-300">
               <li>- Knights always tell the truth.</li>
               <li>- Knaves always lie.</li>
             </ul>
           </div>
           <div className="mb-4">
-            <p className="text-white">Character A: B is a knave.</p>
-            <p className="text-white">Character B: A and I are of opposite types.</p>
+            <p className="text-gray-300">Character A: B is a knave.</p>
+            <p className="text-gray-300">Character B: A and I are of opposite types.</p>
           </div>
         </div>
 
