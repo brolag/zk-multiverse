ce3281dc3862eea0a7bb28a8d6f533ace59c679a
Alfredo Bonilla <info@alfredobonilla.com>
2024-08-08 18:13:35 -0600

    feat: add character components

--- packages/nextjs/app/engineering/circom/[slug]/page.tsx
+++ packages/nextjs/app/engineering/circom/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import fs from "fs/promises";
 import path from "path";
+import Verifier from "~~/components/Verifier";
 import AccordionItem from "~~/components/ui/AccordionItem";
 
 type PageProps = {
@@ -18,7 +19,7 @@ const getSteps = async (slug: string) => {
 };
 
 export default async function Page({ params }: PageProps) {
-  const data = await getSteps(params.slug);
+  const { title, steps } = await getSteps(params.slug);
   const challengeId = params.slug;
 
   return (
@@ -26,9 +27,31 @@ export default async function Page({ params }: PageProps) {
       <div className="w-full max-w-[64rem] p-4 bg-gray-800 rounded-lg">
         <div className="flex items-center mb-4">
           <Image src="/images/mission.png" alt="Mission" width={40} height={40} className="mr-2" />
-          <h2 className="text-3xl font-play font-bold text-white">Challenge #{params.slug} - Circom installation</h2>
+          <h2 className="text-3xl font-play font-bold text-white">
+            Challenge #{params.slug} - {title}
+          </h2>
         </div>
-        {data.map((item: any, index: number) => (
+
+        <div className="w-full max-w-[64rem] p-4 bg-gray-800 rounded-lg mb-6 font-play">
+          <div className="mb-4">
+            <h3 className="text-white text-xl font-bold">Statements</h3>
+            <ul className="text-white">
+              <li>- Knights always tell the truth.</li>
+              <li>- Knaves always lie.</li>
+            </ul>
+          </div>
+          <div className="mb-4">
+            <p className="text-white">Character A: B is a knave.</p>
+            <p className="text-white">Character B: A and I are of opposite types.</p>
+          </div>
+        </div>
+
+        <hr className="border-0 h-px bg-blue-500 shadow-[0_0_10px_2px_rgba(59,130,246,0.7)]" />
+
+        {params.slug !== "1" && <Verifier />}
+        <hr className="border-0 h-px bg-blue-500 shadow-[0_0_10px_2px_rgba(59,130,246,0.7)] mb-10" />
+
+        {steps.map((item: any, index: number) => (
           <AccordionItem
             key={index}
             id={`${index}`}
@@ -39,9 +62,10 @@ export default async function Page({ params }: PageProps) {
               <div>
                 <p>{item.description}</p>
                 <pre>{item.commands.join("\n")}</pre>
-                <a href={`/challenges/challenge_${item.downloadableFile}/files/download.circom`} download>
+                {/* TODO: Adjust the downloadable files */}
+                {/* <a href={/challenges/challenge_${item.downloadableFile}/files/download.circom} download>
                   {item.downloadableFile}
-                </a>
+                </a> */}
               </div>
             }
           />
